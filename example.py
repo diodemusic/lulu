9e1957325eda8c311d24cb5ce1284e6f1defbec1
diodemusic <diodemusicuk@gmail.com>
2023-09-27 13:07:19 +0100

    Update example.py

--- example.py
+++ example.py
@@ -1,13 +1,12 @@
 import lulu.src.lulu as lulu
 
-key = "YOUR_RIOT_API_KEY"
+key = "YOUR_API_KEY"
 
 continent = lulu.continent.europe
 region = lulu.region.euw
 
 player = lulu.account_by_riot_id(key, continent, "saves", "000")
 
-mastery_entries = lulu.mastery_by_puuid(key, region, player.puuid)
-mastery_points_sum = sum(entry.points for entry in mastery_entries)
+sum_score = lulu.mastery_levels_sum_by_puuid(key, region, player.puuid)
 
-print(f"hi im {player.game_name} and i have {mastery_points_sum} total mastery :)")
+print(f"Hi, I'm {player.game_name} and I have {sum_score} total mastery levels :)")
