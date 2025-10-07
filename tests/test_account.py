82365447a5e4af7316fd749608c3097b3f923b30
diodemusic <diodemusicuk@gmail.com>
2025-10-07 20:16:49 +0100

    Initial commit

--- tests/test_account.py
+++ tests/test_account.py
@@ -1,13 +1,19 @@
-from .base import lu  # type: ignore  # noqa: F401
-from lulu.enums.continent import Continent
-from lulu import Lulu
+import lulu
+
+from .base import TEST_PUUID, api  # type: ignore  # noqa: F401
 
 
-def test_account(lu: Lulu):  # noqa: F811
+def test_account_by_puuid(api: lulu.Lulu):  # noqa: F811
+    if not TEST_PUUID:
+        quit()
+
     # Returns AccountDTO {puuid: str, gameName: str?, tagLine: str?}
-    account = lu.account_by_puuid(continent=Continent.EUROPE, puuid="123")
+    account = api.account.by_puuid(
+        continent=lulu.Continent.EUROPE,
+        puuid=TEST_PUUID,
+    )
 
     # assert type(account) == Account
     assert account.game_name == "saves"
     assert account.tag_line == "000"
-    assert account.puuid == "123"
+    assert account.puuid == TEST_PUUID
