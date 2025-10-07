7eeaf77bfdb7d421665f76d83528dc134c7622eb
diodemusic <diodemusicuk@gmail.com>
2025-10-07 16:52:07 +0100

    Add initial Lulu class

--- lulu/main.py
+++ lulu/main.py
@@ -1,3 +1,13 @@
-def say_hi():
-    print("hi")
-    return "hi"
+from .endpoints import account
+from .models.account import Account
+from .enums.continent import Continent
+
+
+class Lulu:
+    def __init__(self, api_key: str):
+        self._api_key = api_key
+
+    def account_by_puuid(self, continent: Continent, puuid: str) -> Account:
+        data = account.by_puuid(continent=continent, puuid=puuid)
+
+        return data
