2a7229cc1ddc3f5a92262d35b5c2fb37f2a351c6
diodemusic <diodemusicuk@gmail.com>
2025-10-07 20:16:36 +0100

    Add TEST_PUUID

--- tests/base.py
+++ tests/base.py
@@ -7,6 +7,15 @@ import lulu
 
 load_dotenv()
 API_KEY = os.getenv("RIOT_API_KEY")
+TEST_PUUID = os.getenv("TEST_PUUID")
+
+if not API_KEY:
+    print("Please add RIOT_API_KEY to .env")
+    quit()
+
+if not TEST_PUUID:
+    print("Please add TEST_PUUID to .env")
+    quit()
 
 
 @pytest.fixture
