77d632e558eb57c17a75ab56e0158fb40a9cf74a
diodemusic <diodemusicuk@gmail.com>
2025-10-07 20:11:18 +0100

    Add Field aliases

--- lulu/models/account.py
+++ lulu/models/account.py
@@ -1,7 +1,9 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class Account(BaseModel):
     puuid: str
-    game_name: str
-    tag_line: str
+    game_name: str = Field(alias="gameName")
+    tag_line: str = Field(alias="tagLine")
+
+    model_config = {"populate_by_name": True}
