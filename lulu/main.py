d0a9cab36c7041dff65d11830f0c6695a2f5d349
diodemusic <diodemusicuk@gmail.com>
2025-10-08 11:33:59 +0100

    Adhere to google docstring style

--- lulu/main.py
+++ lulu/main.py
@@ -2,9 +2,7 @@ from .endpoints.account import AccountEndpoint
 
 
 class Lulu:
-    """
-    Main entrypoint for interacting with the Riot API.
-    """
+    """Main entrypoint for interacting with the Riot API."""
 
     def __init__(self, api_key: str | None):
         self.account = AccountEndpoint(api_key)
