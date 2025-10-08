b33953fc6e1713f7fcabaf5d2541d2b0f98f6a93
diodemusic <diodemusicuk@gmail.com>
2025-10-08 10:40:33 +0100

    Add docstring

--- lulu/main.py
+++ lulu/main.py
@@ -2,5 +2,9 @@ from .endpoints.account import AccountEndpoint
 
 
 class Lulu:
+    """
+    Main entrypoint for interacting with the Riot API.
+    """
+
     def __init__(self, api_key: str | None):
         self.account = AccountEndpoint(api_key)
