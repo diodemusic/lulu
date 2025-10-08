0bd13e92b54f74142217d5dbdfd79de994e7ca80
diodemusic <diodemusicuk@gmail.com>
2025-10-08 17:24:12 +0100

    Make some values optional

--- lulu/models/champion_mastery.py
+++ lulu/models/champion_mastery.py
@@ -1,14 +1,25 @@
+from typing import Optional
+
 from pydantic import BaseModel, Field
 
 
+class RewardConfig(BaseModel):
+    reward_value: str = Field(alias="rewardValue")
+    reward_type: str = Field(alias="rewardType")
+    maximum_reward: int = Field(alias="maximumReward")
+
+
 class NextSeasonMilestones(BaseModel):
-    pass
+    require_grade_counts: object = Field(alias="requireGradeCounts")
+    reward_marks: int = Field(alias="rewardMarks")
+    bonus: bool
+    reward_config: Optional[RewardConfig] = Field(alias="rewardConfig", default=None)
 
 
 class ChampionMastery(BaseModel):
     puuid: str
     champion_points_until_next_level: int = Field(alias="championPointsUntilNextLevel")
-    chest_granted: bool = Field(alias="chestGranted")
+    chest_granted: Optional[bool] = Field(alias="chestGranted", default=None)
     champion_id: int = Field(alias="championId")
     last_play_time: int = Field(alias="lastPlayTime")
     champion_level: int = Field(alias="championLevel")
@@ -16,8 +27,10 @@ class ChampionMastery(BaseModel):
     champion_points_since_last_level: int = Field(alias="championPointsSinceLastLevel")
     mark_required_for_next_level: int = Field(alias="markRequiredForNextLevel")
     champion_season_milestone: int = Field(alias="championSeasonMilestone")
-    next_season_milestone: "NextSeasonMilestones" = Field(alias="nextSeasonMilestone")
+    next_season_milestones: Optional[NextSeasonMilestones] = Field(
+        alias="nextSeasonMilestone", default=None
+    )
     tokens_earned: int = Field(alias="tokensEarned")
-    milestone_grades: list[str] = Field(alias="milestoneGrades")
+    milestone_grades: Optional[list[str]] = Field(alias="milestoneGrades", default=None)
 
     model_config = {"populate_by_name": True}
