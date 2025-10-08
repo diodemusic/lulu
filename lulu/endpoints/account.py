681fd00995006cd742aebf10ffcd8142fba100e7
diodemusic <diodemusicuk@gmail.com>
2025-10-08 11:12:46 +0100

    Add by_riot_id endpoint

--- lulu/endpoints/account.py
+++ lulu/endpoints/account.py
@@ -12,14 +12,33 @@ class AccountEndpoint:
         Get account by puuid.
 
         Args:
-            continent (Continent): Region to execute against.
+            continent (Continent): Continent to execute against.
             puuid (str): Encrypted PUUID. Exact length of 78 characters.
 
         Returns:
-            Account: Instance of the models.account.Account model.
+            Account: Instance of the lulu.models.account.Account model.
         """
 
         path = f"/riot/account/v1/accounts/by-puuid/{puuid}"
-        data = self.client.continent_request(continent, path)
+        data = self.client.continent_request(continent=continent, path=path)
+
+        return Account(**data)
+
+    def by_riot_id(
+        self, continent: Continent, game_name: str, tag_line: str
+    ) -> Account:
+        """
+        Get account by riot id.
+
+        Args:
+            continent (Continent): Continent to execute against.
+            game_name (str): Riot id game name.
+            tag_line (str): Riot id tag line.
+
+        Returns:
+            Account: Instance of the lulu.models.account.Account model.
+        """
+        path = f"/riot/account/v1/accounts/by-riot-id/{game_name}/{tag_line}"
+        data = self.client.continent_request(continent=continent, path=path)
 
         return Account(**data)
--- tests/test_account.py
+++ tests/test_account.py
@@ -17,3 +17,17 @@ def test_account_by_puuid(api: lulu.Lulu):  # noqa: F811
     assert account.game_name == "saves"
     assert account.tag_line == "000"
     assert account.puuid == TEST_PUUID
+
+
+def test_account_by_riot_id(api: lulu.Lulu):  # noqa: F811
+    if not TEST_PUUID:
+        quit()
+
+    account = api.account.by_riot_id(
+        continent=lulu.Continent.EUROPE, game_name="saves", tag_line="000"
+    )
+
+    assert type(account) is Account
+    assert account.game_name == "saves"
+    assert account.tag_line == "000"
+    assert account.puuid == TEST_PUUID
