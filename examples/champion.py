4a32dd27a29d32fb787b43b6d34bf23339318767
diodemusic <diodemusicuk@gmail.com>
2025-10-08 21:59:41 +0100

    Add champion.rotations

---
+++ examples/champion.py
@@ -0,0 +1,17 @@
+import os
+
+from dotenv import load_dotenv
+
+import lulu
+
+load_dotenv()
+API_KEY = os.getenv("RIOT_API_KEY")
+api = lulu.Lulu(API_KEY)
+
+rotations = api.champion.rotations(lulu.Region.EUW)
+
+print(f"Max new player level: {rotations.max_new_player_level}")
+print(
+    f"Free champion ids for new players: {rotations.free_champion_ids_for_new_players}"
+)
+print(f"Free champion ids: {rotations.free_champion_ids}")
