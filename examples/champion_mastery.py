7e4d0e14ab555178fbd4f0fe52a5f8ff07a2dd2d
diodemusic <diodemusicuk@gmail.com>
2025-10-08 19:45:34 +0100

    Add champion_mastery.by_puuid_and_champion_id example

--- examples/champion_mastery.py
+++ examples/champion_mastery.py
@@ -19,3 +19,17 @@ for champion_mastery in champion_masteries:
     print(f"Champion Points: {champion_mastery.champion_points}")
 
     print("-" * 50)
+
+CHAMPION_ID = 11
+
+try:
+    champion_mastery = api.champion_mastery.by_puuid_and_champion_id(
+        REGION, account.puuid, CHAMPION_ID
+    )
+except lulu.exceptions.DataNotFound as e:
+    print(f"Champion id {CHAMPION_ID} not found: {e}")
+    quit()
+
+print(f"Champion ID: {champion_mastery.champion_id}")
+print(f"Champion Level: {champion_mastery.champion_level}")
+print(f"Champion Points: {champion_mastery.champion_points}")
