91c0f33445a19e1e640b3b9f4705e0c2f2431d8f
diodemusic <diodemusicuk@gmail.com>
2025-10-08 19:45:49 +0100

    Test champion_mastery.by_puuid_and_champion_id

--- tests/test_champion_mastery.py
+++ tests/test_champion_mastery.py
@@ -15,3 +15,13 @@ def test_champion_mastery_masteries_by_puuid(api: lulu.Lulu):  # noqa: F811
 
     for champion_mastery in champion_masteries:
         assert isinstance(champion_mastery, ChampionMastery)
+
+
+def test_champion_mastery_by_puuid_and_champion_id(api: lulu.Lulu):  # noqa: F811
+    champion_mastery_by_puuid_and_champion_id = (
+        api.champion_mastery.by_puuid_and_champion_id(
+            region=lulu.Region.EUW, puuid=TEST_PUUID, champion_id=11
+        )
+    )
+
+    assert isinstance(champion_mastery_by_puuid_and_champion_id, ChampionMastery)
