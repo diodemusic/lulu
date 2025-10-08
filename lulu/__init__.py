caa4b4b452ae01b6d4620e5c104a5f924e81675b
diodemusic <diodemusicuk@gmail.com>
2025-10-08 11:33:47 +0100

    Expose Region enum

--- lulu/__init__.py
+++ lulu/__init__.py
@@ -1,2 +1,3 @@
 from .enums.continent import Continent  # type: ignore  # noqa: F401
+from .enums.region import Region  # type: ignore  # noqa: F401
 from .main import Lulu  # type: ignore  # noqa: F401
