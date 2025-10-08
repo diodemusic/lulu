ab1ccd70d80082578729896466f9a7055f95a533
diodemusic <diodemusicuk@gmail.com>
2025-10-08 14:12:11 +0100

    Expose exceptions

--- lulu/__init__.py
+++ lulu/__init__.py
@@ -1,3 +1,4 @@
+from . import exceptions  # type: ignore  # noqa: F401
 from .enums.continent import Continent  # type: ignore  # noqa: F401
 from .enums.region import Region  # type: ignore  # noqa: F401
 from .main import Lulu  # type: ignore  # noqa: F401
