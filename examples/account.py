e87a7c81e32fc6269ab1098cf45d58e532e71a43
diodemusic <diodemusicuk@gmail.com>
2025-10-08 12:57:13 +0100

    Add account example

---
+++ examples/account.py
@@ -0,0 +1,22 @@
+import os
+
+from dotenv import load_dotenv
+
+import lulu
+
+load_dotenv()
+API_KEY = os.getenv("RIOT_API_KEY")
+api = lulu.Lulu(API_KEY)
+CONTINENT = lulu.Continent.EUROPE
+
+account = api.account.by_riot_id(CONTINENT, "saves", "000")
+
+print(f"PUUID: {account.puuid}")
+print(f"Game name: {account.game_name}")
+print(f"Tag line: {account.tag_line}")
+
+region = api.account.region_by_puuid(CONTINENT, account.puuid)
+
+print(f"PUUID: {region.puuid}")
+print(f"Game: {region.game}")
+print(f"Region: {region.region.value}")
