9bf4ac3b46fdc3dd396a689cb5dad7de445d3eff
diodemusic <diodemusicuk@gmail.com>
2025-10-08 11:34:58 +0100

    Test account.region_by_puuid

--- tests/test_account.py
+++ tests/test_account.py
@@ -1,5 +1,5 @@
 import lulu
-from lulu.models.account import Account
+from lulu.models.account import Account, AccountRegion
 
 from .base import TEST_PUUID, api  # type: ignore  # noqa: F401
 
@@ -31,3 +31,20 @@ def test_account_by_riot_id(api: lulu.Lulu):  # noqa: F811
     assert account.game_name == "saves"
     assert account.tag_line == "000"
     assert account.puuid == TEST_PUUID
+
+
+def test_account_region_by_puuid(api: lulu.Lulu):  # noqa: F811
+    if not TEST_PUUID:
+        quit()
+
+    account_region = api.account.region_by_puuid(
+        continent=lulu.Continent.EUROPE, puuid=TEST_PUUID
+    )
+
+    assert type(account_region) is AccountRegion
+    assert account_region.puuid == TEST_PUUID
+    assert account_region.game == "lol"
+
+    assert type(account_region.region) is lulu.Region
+    assert account_region.region == lulu.Region.EUW
+    assert account_region.region.value == "euw1"
