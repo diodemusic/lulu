dd448cedecdfc6d9febf4354567518ccab6acf20
diodemusic <diodemusicuk@gmail.com>
2025-10-08 10:40:58 +0100

    Assert account type

--- tests/test_account.py
+++ tests/test_account.py
@@ -1,4 +1,5 @@
 import lulu
+from lulu.models.account import Account
 
 from .base import TEST_PUUID, api  # type: ignore  # noqa: F401
 
@@ -7,13 +8,12 @@ def test_account_by_puuid(api: lulu.Lulu):  # noqa: F811
     if not TEST_PUUID:
         quit()
 
-    # Returns AccountDTO {puuid: str, gameName: str?, tagLine: str?}
     account = api.account.by_puuid(
         continent=lulu.Continent.EUROPE,
         puuid=TEST_PUUID,
     )
 
-    # assert type(account) == Account
+    assert type(account) is Account
     assert account.game_name == "saves"
     assert account.tag_line == "000"
     assert account.puuid == TEST_PUUID
