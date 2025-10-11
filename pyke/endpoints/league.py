1bde9a096a75447c90dd2d08b684409ac228003d
diodemusic <diodemusicuk@gmail.com>
2025-10-11 23:10:13 +0100

    Add by_queue_tier_division

--- pyke/endpoints/league.py
+++ pyke/endpoints/league.py
@@ -1,4 +1,4 @@
-from pyke import Queue, Region
+from pyke import Division, Queue, Region, Tier
 
 from .._base_client import _BaseApiClient
 from .._models.league_v4 import LeagueEntryDTO, LeagueListDTO
@@ -41,6 +41,38 @@ class LeagueEndpoint:
 
         path = f"/lol/league/v4/entries/by-puuid/{puuid}"
         data = self._client._region_request(region=region, path=path)
+
+        league_entries: list[LeagueEntryDTO] = []
+
+        for league_entry in data:
+            league_entries.append(LeagueEntryDTO(**league_entry))
+
+        return league_entries
+
+    def by_queue_tier_division(
+        self,
+        region: Region,
+        queue: Queue,
+        tier: Tier,
+        division: Division,
+        page: int = 1,
+    ) -> list[LeagueEntryDTO]:
+        """Get all the league entries.
+
+        Args:
+            region (Region): Region to execute against (pyke.enums.region.Region).
+            queue (Queue): Ranked queue type (pyke.enums.queue.Queue).
+            tier (Tier): Ranked tier (pyke.enums.tier.Tier).
+            division (Division): Ranked division (pyke.enums.division.Division).
+            page (int, optional): Starts with page 1. Defaults to 1.
+
+        Returns:
+            list[LeagueEntryDTO]: List of pyke.models.league_v4.LeagueEntryDTO objects.
+        """
+
+        path = f"/lol/league/v4/entries/{queue.value}/{tier.value}/{division.value}"
+        params = {"count": page}
+        data = self._client._region_request(region=region, path=path, params=params)
 
         league_entries: list[LeagueEntryDTO] = []
 
--- tests/test_league.py
+++ tests/test_league.py
@@ -1,4 +1,4 @@
-from pyke import Pyke, Queue, Region
+from pyke import Division, Pyke, Queue, Region, Tier
 from pyke._models.league_v4 import LeagueEntryDTO, LeagueListDTO
 
 from .base import TEST_PUUID, api
@@ -19,4 +19,20 @@ def test_by_puuid(api: Pyke):
     by_puuid = api.league.by_puuid(region=Region.EUW, puuid=TEST_PUUID)
 
     for league_entry in by_puuid:
+        assert isinstance(league_entry, LeagueEntryDTO)
+
+
+def test_by_queue_tier_division(api: Pyke):
+    by_queue_tier_division = api.league.by_queue_tier_division(
+        Region.EUW, Queue.SOLO_DUO, Tier.GOLD, Division.II, page=2
+    )
+
+    for league_entry in by_queue_tier_division:
+        assert isinstance(league_entry, LeagueEntryDTO)
+
+    by_queue_tier_division = api.league.by_queue_tier_division(
+        Region.EUW, Queue.SOLO_DUO, Tier.GOLD, Division.II
+    )
+
+    for league_entry in by_queue_tier_division:
         assert isinstance(league_entry, LeagueEntryDTO)
