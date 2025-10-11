c0ea1c5858bb1f64b585bc291fffbf4c5488b9f2
diodemusic <diodemusicuk@gmail.com>
2025-10-11 21:05:03 +0100

    rename test_entries_by_queue_tier_division

--- tests/test_league_exp.py
+++ tests/test_league_exp.py
@@ -4,7 +4,7 @@ from pyke._models.league_exp_v4 import LeagueEntryDTO
 from .base import api  # type: ignore  # noqa: F401
 
 
-def test_champion_rotation(api: Pyke):  # noqa: F811
+def test_entries_by_queue_tier_division(api: Pyke):  # noqa: F811
     league_entries = api.league_exp.entries_by_queue_tier_division(
         Region.EUW, Queue.SOLO_DUO, Tier.GOLD, Division.II, page=2
     )
