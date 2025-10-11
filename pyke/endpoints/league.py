dc7eb1151bf6238998b3a3a09ae6463c728933c4
diodemusic <diodemusicuk@gmail.com>
2025-10-11 22:08:55 +0100

    Add by_puuid

--- pyke/endpoints/league.py
+++ pyke/endpoints/league.py
@@ -1,7 +1,7 @@
 from pyke import Queue, Region
 
 from .._base_client import _BaseApiClient
-from .._models.league_v4 import LeagueListDTO
+from .._models.league_v4 import LeagueEntryDTO, LeagueListDTO
 
 
 class LeagueEndpoint:
@@ -27,3 +27,24 @@ class LeagueEndpoint:
         data = self._client._region_request(region=region, path=path)
 
         return LeagueListDTO(**data)
+
+    def by_puuid(self, region: Region, puuid: str) -> list[LeagueEntryDTO]:
+        """Get league entries in all queues for a given puuid.
+
+        Args:
+            region (Region): Region to execute against (pyke.enums.region.Region).
+            puuid (str): Encrypted PUUID. Exact length of 78 characters.
+
+        Returns:
+            list[LeagueEntryDTO]: List of pyke.models.league_exp_v4.LeagueEntryDTO objects.
+        """
+
+        path = f"/lol/league/v4/entries/by-puuid/{puuid}"
+        data = self._client._region_request(region=region, path=path)
+
+        league_entries: list[LeagueEntryDTO] = []
+
+        for league_entry in data:
+            league_entries.append(LeagueEntryDTO(**league_entry))
+
+        return league_entries
--- tests/test_league.py
+++ tests/test_league.py
@@ -1,16 +1,22 @@
 from pyke import Pyke, Queue, Region
-from pyke._models.league_v4 import LeagueListDTO
+from pyke._models.league_v4 import LeagueEntryDTO, LeagueListDTO
 
-from .base import api
+from .base import TEST_PUUID, api
+
+if not TEST_PUUID:
+    quit()
 
 
 def test_challenger_leagues_by_queue(api: Pyke):
     challenger_leagues_by_queue = api.league.challenger_leagues_by_queue(
-        Region.EUW, Queue.SOLO_DUO
+        region=Region.EUW, queue=Queue.SOLO_DUO
     )
 
     assert isinstance(challenger_leagues_by_queue, LeagueListDTO)
 
 
 def test_by_puuid(api: Pyke):
-    pass
+    by_puuid = api.league.by_puuid(region=Region.EUW, puuid=TEST_PUUID)
+
+    for league_entry in by_puuid:
+        assert isinstance(league_entry, LeagueEntryDTO)
