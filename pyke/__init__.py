2465afa8113f233623e1230fc9a623b1a6ee40de
diodemusic <diodemusicuk@gmail.com>
2025-10-11 17:43:45 +0100

    Add top level lint config

--- pyke/__init__.py
+++ pyke/__init__.py
@@ -1,7 +1,9 @@
-from . import exceptions  # type: ignore  # noqa: F401
-from .enums.continent import Continent  # type: ignore  # noqa: F401
-from .enums.division import Division  # type: ignore  # noqa: F401
-from .enums.queue import Queue  # type: ignore  # noqa: F401
-from .enums.region import Region  # type: ignore  # noqa: F401
-from .enums.tier import Tier  # type: ignore  # noqa: F401
-from .main import Pyke  # type: ignore  # noqa: F401
+# pyright: reportUnusedImport=false
+# ruff: noqa: F401
+from . import exceptions
+from .enums.continent import Continent
+from .enums.division import Division
+from .enums.queue import Queue
+from .enums.region import Region
+from .enums.tier import Tier
+from .main import Pyke
