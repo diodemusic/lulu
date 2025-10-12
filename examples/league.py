b4f8dcca0b90b293e0e3a17a3bef3dfe560a4dec
diodemusic <diodemusicuk@gmail.com>
2025-10-13 00:50:09 +0100

    Calculate win rates

--- examples/league.py
+++ examples/league.py
@@ -17,8 +17,14 @@ for challenger_player in challenger_leagues.entries[0:5]:
     account = api.account.by_puuid(
         continent=Continent.EUROPE, puuid=challenger_player.puuid
     )
+
     riot_id = f"{account.game_name}#{account.tag_line}"
 
+    wins = challenger_player.wins
+    losses = challenger_player.losses
+    total_games_played = wins + losses
+    win_rate = (wins / total_games_played) * 100
+
     print(
-        f"{riot_id} has {challenger_player.wins} wins and {challenger_player.losses} losses."
+        f"{riot_id} has {challenger_player.league_points} LP with {wins} wins and {losses} losses ({win_rate:.2f}% wr)."
     )
