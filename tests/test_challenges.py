ac4b691bf136069668c1f2fb847a7ff9602e4b06
diodemusic <diodemusicuk@gmail.com>
2025-10-13 22:45:54 +0100

    Add some red tests

--- tests/test_challenges.py
+++ tests/test_challenges.py
@@ -9,3 +9,23 @@ def test_config(api: Pyke):
 
     for challenge in config:
         assert isinstance(challenge, ChallengeConfigInfoDto)
+
+
+def test_percentiles(api: Pyke):
+    percentiles = api.challenges.percentiles(region=Region.EUW)
+
+    assert type(percentiles) is dict
+
+
+def test_config_by_challenge_id(api: Pyke):
+    config_by_challenge_id = api.challenges.config_by_challenge_id(
+        region=Region.EUW, challenge_id=TEST_CHELLENGE_ID
+    )
+
+    assert isinstance(config_by_challenge_id, ChallengeConfigInfoDto)
+
+
+def test_leaderboards_by_level(api: Pyke):
+    leaderboards_by_level = api.challenges.leaderboards_by_level(
+        region=Region.EUW, level=Level.PLATINUM, challenge_id=TEST_CHALLENGE_ID
+    )
