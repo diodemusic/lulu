989bdb2fb62264b16ceffeea039a393d0f161393
diodemusic <diodemusicuk@gmail.com>
2025-10-14 17:07:10 +0100

    Rename functions

--- tests/test_champion_mastery.py
+++ tests/test_champion_mastery.py
@@ -7,51 +7,47 @@ if not TEST_PUUID:
     quit()
 
 
-def test_champion_mastery_masteries_by_puuid(api: Pyke):
-    champion_mastery_masteries_by_puuid = api.champion_mastery.masteries_by_puuid(
+def test_masteries_by_puuid(api: Pyke):
+    _masteries_by_puuid = api.champion_mastery.masteries_by_puuid(
         region=Region.EUW,
         puuid=TEST_PUUID,
     )
 
-    for champion_mastery in champion_mastery_masteries_by_puuid:
+    for champion_mastery in _masteries_by_puuid:
         assert isinstance(champion_mastery, ChampionMasteryDto)
 
 
-def test_champion_mastery_by_puuid_and_champion_id(api: Pyke):
-    champion_mastery_by_puuid_and_champion_id = (
-        api.champion_mastery.by_puuid_and_champion_id(
-            region=Region.EUW, puuid=TEST_PUUID, champion_id=11
-        )
+def test_by_puuid_and_champion_id(api: Pyke):
+    by_puuid_and_champion_id = api.champion_mastery.by_puuid_and_champion_id(
+        region=Region.EUW, puuid=TEST_PUUID, champion_id=11
     )
 
-    assert isinstance(champion_mastery_by_puuid_and_champion_id, ChampionMasteryDto)
+    assert isinstance(by_puuid_and_champion_id, ChampionMasteryDto)
 
 
-def test_champion_mastery_masteries_by_puuid_top(api: Pyke):
-    champion_mastery_masteries_by_puuid_top = (
-        api.champion_mastery.masteries_by_puuid_top(
-            region=Region.EUW, puuid=TEST_PUUID, count=10
-        )
+def test_masteries_by_puuid_top(api: Pyke):
+    masteries_by_puuid_top = api.champion_mastery.masteries_by_puuid_top(
+        region=Region.EUW, puuid=TEST_PUUID, count=10
     )
 
-    assert len(champion_mastery_masteries_by_puuid_top) == 10
+    assert len(masteries_by_puuid_top) == 10
 
-    for champion_mastery in champion_mastery_masteries_by_puuid_top:
+    for champion_mastery in masteries_by_puuid_top:
         assert isinstance(champion_mastery, ChampionMasteryDto)
 
-    champion_mastery_masteries_by_puuid_top = (
-        api.champion_mastery.masteries_by_puuid_top(region=Region.EUW, puuid=TEST_PUUID)
-    )
-
-    assert len(champion_mastery_masteries_by_puuid_top) == 3
-
-    for champion_mastery in champion_mastery_masteries_by_puuid_top:
-        assert isinstance(champion_mastery, ChampionMasteryDto)
-
-
-def test_champion_mastery_score_by_puuid(api: Pyke):
-    champion_mastery_score_by_puuid = api.champion_mastery.score_by_puuid(
+    masteries_by_puuid_top = api.champion_mastery.masteries_by_puuid_top(
         region=Region.EUW, puuid=TEST_PUUID
     )
 
-    assert isinstance(champion_mastery_score_by_puuid, int)
+    assert len(masteries_by_puuid_top) == 3
+
+    for champion_mastery in masteries_by_puuid_top:
+        assert isinstance(champion_mastery, ChampionMasteryDto)
+
+
+def test_score_by_puuid(api: Pyke):
+    score_by_puuid = api.champion_mastery.score_by_puuid(
+        region=Region.EUW, puuid=TEST_PUUID
+    )
+
+    assert isinstance(score_by_puuid, int)
