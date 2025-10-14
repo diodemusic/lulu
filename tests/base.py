d1cef8acb05014c527a92b230bc27a53c3559b4a
diodemusic <diodemusicuk@gmail.com>
2025-10-14 17:06:19 +0100

    Type cast TEST_CHALLENGE_ID to int

--- tests/base.py
+++ tests/base.py
@@ -9,7 +9,10 @@ load_dotenv()
 API_KEY = os.getenv("RIOT_API_KEY")
 TEST_PUUID = os.getenv("TEST_PUUID")
 TEST_LEAGUE_ID = os.getenv("TEST_LEAGUE_ID")
-TEST_CHALLENGE_ID = os.getenv("TEST_CHALLENGE_ID")
+TEST_CHALLENGE_ID_STR = os.getenv("TEST_CHALLENGE_ID")
+if TEST_CHALLENGE_ID_STR:
+    TEST_CHALLENGE_ID = int(TEST_CHALLENGE_ID_STR)
+TEST_MATCH_ID = os.getenv("TEST_MATCH_ID")
 
 
 @pytest.fixture
