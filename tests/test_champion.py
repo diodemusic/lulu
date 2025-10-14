9e4d849dc2f816457ae7cdad34b7ce4a8b8cf935
diodemusic <diodemusicuk@gmail.com>
2025-10-14 17:07:23 +0100

    Rename functions

--- tests/test_champion.py
+++ tests/test_champion.py
@@ -4,9 +4,7 @@ from pyke._models.champion_v3 import ChampionInfo
 from .base import api
 
 
-def test_champion_rotation(api: Pyke):
-    champion_rotation = api.champion.rotations(region=Region.EUW)
+def test_rotations(api: Pyke):
+    rotations = api.champion.rotations(region=Region.EUW)
 
-    print(champion_rotation)
-
-    assert isinstance(champion_rotation, ChampionInfo)
+    assert isinstance(rotations, ChampionInfo)
