04209b4c035161209ed2a58684780d7a6e51ec8d
diodemusic <diodemusicuk@gmail.com>
2025-10-14 17:05:09 +0100

    Expose Level enum

--- pyke/__init__.py
+++ pyke/__init__.py
@@ -3,6 +3,7 @@
 from . import exceptions
 from .enums.continent import Continent
 from .enums.division import Division
+from .enums.level import Level
 from .enums.queue import Queue
 from .enums.region import Region
 from .enums.tier import Tier
