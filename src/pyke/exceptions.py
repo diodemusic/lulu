ccfcd23ef8781871380b9c918a86ca45e0c615f2
diodemusic <diodemusicuk@gmail.com>
2025-10-15 20:34:28 +0100

    Add UknownError

--- src/pyke/exceptions.py
+++ src/pyke/exceptions.py
@@ -106,3 +106,13 @@ class GatewayTimeout(Exception):
 
     def __str__(self):
         return f"{self.message} (Error Code: {self.error_code})"
+
+
+class UnknownError(Exception):
+    def __init__(self, message: str, error_code: int):
+        super().__init__(message)
+        self.message = message
+        self.error_code = error_code
+
+    def __str__(self):
+        return f"{self.message} (Error Code: {self.error_code})"
