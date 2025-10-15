1c3b5d051974963d51d2f2c7ed1dbec5d65e8d99
diodemusic <diodemusicuk@gmail.com>
2025-10-15 16:11:11 +0100

    Hint enums as private

--- pyke/_base_client.py
+++ pyke/_base_client.py
@@ -3,8 +3,8 @@ from typing import Any
 import requests
 
 from . import exceptions
-from .enums.continent import Continent
-from .enums.region import Region
+from ._enums.continent import Continent
+from ._enums.region import Region
 
 
 class _BaseApiClient:  # pyright: ignore[reportUnusedClass]
