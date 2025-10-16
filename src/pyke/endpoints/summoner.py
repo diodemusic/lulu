f836476d7e5346c125fff451d3ef0963736338ba
diodemusic <diodemusicuk@gmail.com>
2025-10-16 17:49:25 +0100

    Add summoner endpoint

--- src/pyke/endpoints/summoner.py
+++ src/pyke/endpoints/summoner.py
@@ -1,6 +1,25 @@
+from pyke import Region
+
 from .._base_client import _BaseApiClient
+from .._models.summoner_v4 import SummonerDTO
 
 
 class SummonerEndpoint:
     def __init__(self, client: _BaseApiClient):
         self._client = client
+
+    def by_puuid(self, region: Region, puuid: str) -> SummonerDTO:
+        """Get a summoner by PUUID.
+
+        **Args:**  
+            `region (Region):` Region to execute against (pyke._enums.region.Region).  
+            `puuid (str):` Encrypted PUUID. Exact length of 78 characters.  
+
+        **Returns:**  
+            `CurrentGameInfo:` pyke._models.summoner_v4.SummonerDTO
+        """  # fmt: skip
+
+        path = f"/lol/summoner/v4/summoners/by-puuid/{puuid}"
+        data = self._client._region_request(region=region, path=path)
+
+        return SummonerDTO(**data)
