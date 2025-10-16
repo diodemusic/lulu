7b57b81c3a0fd37e77ea4e2dd8083a298692a376
diodemusic <diodemusicuk@gmail.com>
2025-10-16 17:39:10 +0100

    Add status example

---
+++ examples/status.py
@@ -0,0 +1,24 @@
+import os
+
+from dotenv import load_dotenv
+
+from pyke import Pyke, Region
+
+load_dotenv()
+API_KEY = os.getenv("RIOT_API_KEY")
+api = Pyke(API_KEY)
+
+status = api.status.platform_data(Region.EUW)
+
+for incident in status.incidents:
+    if incident.incident_severity:
+        print(
+            f"{incident.incident_severity.name} incident: {incident.titles[0].content}"
+        )
+
+        print("Platforms affected:")
+
+        for platform in incident.platforms:
+            print(platform.name)
+
+        print("-" * 20)
