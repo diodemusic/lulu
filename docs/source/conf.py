28bc59fbc795f912b4f950a00adce88925eb3040
diodemusic <diodemusicuk@gmail.com>
2025-10-17 22:35:03 +0100

    Add docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,22 +12,13 @@ import sys
 sys.path.insert(0, os.path.abspath("../../src"))
 
 project = "pyke-lol"
-author = "Your Name"
+author = "diodemusic"
+release = "1.1.0"
 
-try:
-    from pyke import __author__, __version__
-except ImportError:
-    __version__ = "1.x.x"
-    __author__ = "diodemusic"
-
-release = __version__
-
-# Sphinx extensions
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
-    "sphinx_autodoc_typehints",
 ]
 
 html_theme = "sphinx_rtd_theme"
