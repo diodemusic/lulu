8a3f8fd9df32719d708bf64f94391acff9924864
diodemusic <diodemusicuk@gmail.com>
2025-10-18 23:51:41 +0100

    Add champion_mastery example

--- examples/champion_mastery.py
+++ examples/champion_mastery.py
@@ -7,45 +7,22 @@ from pyke import Continent, Pyke, Region, exceptions
 load_dotenv()
 API_KEY = os.getenv("RIOT_API_KEY")
 api = Pyke(API_KEY)
-REGION = Region.EUW
 
+# Let's get my puuid
 account = api.account.by_riot_id(Continent.EUROPE, "saves", "000")
 
-champion_masteries = api.champion_mastery.masteries_by_puuid(REGION, account.puuid)
+# Now my champion masteries
+champion_masteries = api.champion_mastery.masteries_by_puuid(Region.EUW, account.puuid)
 
-for champion_mastery in champion_masteries:
+
+# Let's print my top ten champion masteries
+for champion_mastery in champion_masteries[:10]:
     print(f"Champion ID: {champion_mastery.champion_id}")
     print(f"Champion Level: {champion_mastery.champion_level}")
     print(f"Champion Points: {champion_mastery.champion_points}")
 
     print("-" * 50)
 
-CHAMPION_ID = 11
-
-try:
-    champion_mastery = api.champion_mastery.by_puuid_and_champion_id(
-        REGION, account.puuid, CHAMPION_ID
-    )
-except exceptions.DataNotFound as e:
-    print(f"Champion id {CHAMPION_ID} not found: {e}")
-    quit()
-
-print(f"Champion ID: {champion_mastery.champion_id}")
-print(f"Champion Level: {champion_mastery.champion_level}")
-print(f"Champion Points: {champion_mastery.champion_points}")
-
-champion_masteries = api.champion_mastery.masteries_by_puuid_top(
-    REGION, account.puuid, count=5
-)
-
-for champion_mastery in champion_masteries:
-    print(f"Champion ID: {champion_mastery.champion_id}")
-    print(f"Champion Level: {champion_mastery.champion_level}")
-    print(f"Champion Points: {champion_mastery.champion_points}")
-
-    print("-" * 50)
-
-
-score = api.champion_mastery.score_by_puuid(REGION, account.puuid)
-
+# And we can get my total mastery score
+score = api.champion_mastery.score_by_puuid(Region.EUW, account.puuid)
 print(f"Player's total champion mastery score: {score}")
