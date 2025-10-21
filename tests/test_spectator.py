9b39cfbf898c57820c2a38826a3d73947457cacf
diodemusic <diodemusicuk@gmail.com>
2025-10-21 20:54:48 +0100

    Skip spectator-v5 test

--- tests/test_spectator.py
+++ tests/test_spectator.py
@@ -1,13 +1,13 @@
-from pyke import Pyke, Region
-from pyke.models.spectator_v5 import CurrentGameInfo
+# from pyke import Pyke, Region
+# from pyke.models.spectator_v5 import CurrentGameInfo
 
-from .base import TEST_PUUID, api
+# from .base import TEST_PUUID, api
 
 
-def test_by_puuid(api: Pyke):
-    by_puuid = api.spectator.by_puuid(
-        region=Region.EUW,
-        puuid=TEST_PUUID,
-    )
+# def test_by_puuid(api: Pyke):
+#     by_puuid = api.spectator.by_puuid(
+#         region=Region.EUW,
+#         puuid=TEST_PUUID,
+#     )
 
-    assert isinstance(by_puuid, CurrentGameInfo)
+#     assert isinstance(by_puuid, CurrentGameInfo)
