c1cec48539d114a5ae1263a916486c795f06bf72
diodemusic <diodemusicuk@gmail.com>
2025-10-22 10:23:16 +0100

    Add Region test

---
+++ tests/test_regions.py
@@ -0,0 +1,11 @@
+from pyke import Pyke, Queue, Region
+from pyke.models.league_v4 import LeagueListDTO
+
+from .base import TEST_PUUID, api
+
+
+def test_regions(api: Pyke):
+    for region in Region:
+        league = api.league.challenger_leagues_by_queue(region, Queue.SOLO_DUO)
+
+        assert isinstance(league, LeagueListDTO)
