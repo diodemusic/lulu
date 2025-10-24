f94eb5b08bdf13d3d3bada819967393dae9728ac
diodemusic <diodemusicuk@gmail.com>
2025-10-24 16:01:16 +0100

    Add return type hints

--- src/pyke/_base_client.py
+++ src/pyke/_base_client.py
@@ -13,7 +13,7 @@ class _BaseApiClient:  # pyright: ignore[reportUnusedClass]
     CONTINENT_BASE = "https://{continent}.api.riotgames.com"
     REGION_BASE = "https://{region}.api.riotgames.com"
 
-    def __init__(self, api_key: str | None, print_url: bool):
+    def __init__(self, api_key: str | None, print_url: bool) -> None:
         self.api_key = api_key
         self.print_url = print_url
         self._status_code_registry = {
@@ -29,7 +29,7 @@ class _BaseApiClient:  # pyright: ignore[reportUnusedClass]
             504: exceptions.GatewayTimeout("Gateway timeout", 504),
         }
 
-    def _print_url(self, response: Response, url: str):
+    def _print_url(self, response: Response, url: str) -> None:
         if self.print_url:
             count = response.headers.get("X-App-Rate-Limit-Count", "unknown").split(
                 ":"
@@ -37,18 +37,18 @@ class _BaseApiClient:  # pyright: ignore[reportUnusedClass]
             limit = response.headers.get("X-App-Rate-Limit", "unknown").split(":")[0]
             print(f"({count}/{limit}) - {url}")
 
-    def _retry_after(self, response: Response):
+    def _retry_after(self, response: Response) -> None:
         retry_after = int(response.headers.get("Retry-After", "120"))
         print(f"Rate limit exceeded, waiting {retry_after} seconds")
         time.sleep(retry_after)
 
-    def _response_json(self, response: Response):
+    def _response_json(self, response: Response) -> Any:
         try:
             return response.json()
         except ValueError:
             raise exceptions.InternalServerError("Empty JSON response", 500)
 
-    def _get(self, url: str, params: dict[Any, Any] | None = None):
+    def _get(self, url: str, params: dict[Any, Any] | None = None) -> Any:
         while True:
             headers = {"X-Riot-Token": self.api_key}
             response = requests.get(url, headers=headers, params=params, timeout=30)
@@ -71,12 +71,12 @@ class _BaseApiClient:  # pyright: ignore[reportUnusedClass]
 
     def _continent_request(
         self, continent: Continent, path: str, params: dict[Any, Any] | None = None
-    ):
+    ) -> Any:
         url = f"{self.CONTINENT_BASE.format(continent=continent.value)}{path}"
         return self._get(url, params)
 
     def _region_request(
         self, region: Region, path: str, params: dict[Any, Any] | None = None
-    ):
+    ) -> Any:
         url = f"{self.REGION_BASE.format(region=region.value)}{path}"
         return self._get(url, params)
