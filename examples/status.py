ca80a35c92f95c43e277f88b0033c5651cc45e7a
diodemusic <diodemusicuk@gmail.com>
2025-10-27 19:41:29 +0000

    Update status.py

--- examples/status.py
+++ examples/status.py
@@ -8,8 +8,10 @@ load_dotenv()
 API_KEY = os.getenv("RIOT_API_KEY")
 api = Pyke(API_KEY)
 
+# Does EUW have any issues or maintenance right now?
 status = api.lol_status.platform_data(Region.EUW)
 
+# If there are any incidents we will print the incident in english
 for incident in status.incidents:
     if incident.incident_severity:
         print(
