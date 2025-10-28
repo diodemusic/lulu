2ee769031a8a211fa25d7c8c3710022d7a5b069e
diodemusic <diodemusicuk@gmail.com>
2025-10-28 01:49:52 +0000

    Update variable names

--- examples/champion_mastery.py
+++ examples/champion_mastery.py
@@ -12,17 +12,17 @@ api = Pyke(API_KEY)
 account = api.account.by_riot_id(Continent.EUROPE, "saves", "000")
 
 # Now my champion masteries
-champion_masteries = api.champion_mastery.masteries_by_puuid(Region.EUW, account.puuid)
+masteries = api.champion_mastery.masteries_by_puuid(Region.EUW, account.puuid)
 
 
 # Let's print my top ten champion masteries
-for champion_mastery in champion_masteries[:10]:
-    print(f"Champion ID: {champion_mastery.champion_id}")
-    print(f"Champion Level: {champion_mastery.champion_level}")
-    print(f"Champion Points: {champion_mastery.champion_points}")
+for mastery in masteries[:10]:
+    print(f"Champion ID: {mastery.champion_id}")
+    print(f"Champion Level: {mastery.champion_level}")
+    print(f"Champion Points: {mastery.champion_points}")
 
     print("-" * 50)
 
-# And we can get my total mastery score
+# And we can get my total champion mastery score
 score = api.champion_mastery.score_by_puuid(Region.EUW, account.puuid)
 print(f"Player's total champion mastery score: {score}")
