91bfeaa07bba53f5b2f25ef5648fe3aa161d7dc4
diodemusic <diodemusicuk@gmail.com>
2025-10-28 01:50:00 +0000

    Add comments

--- examples/champion.py
+++ examples/champion.py
@@ -8,10 +8,14 @@ load_dotenv()
 API_KEY = os.getenv("RIOT_API_KEY")
 api = Pyke(API_KEY)
 
+# Let's get the current champions in the free rotation
 rotations = api.champion.rotations(Region.EUW)
 
 print(f"Max new player level: {rotations.max_new_player_level}")
+
+# The free champions for new players are different
 print(
     f"Free champion ids for new players: {rotations.free_champion_ids_for_new_players}"
 )
+
 print(f"Free champion ids: {rotations.free_champion_ids}")
