318b86077564179a4552eea61dbbd214dcb4b1e8
diodemusic <diodemusicuk@gmail.com>
2025-11-01 18:27:19 +0000

    Add ddragon example

---
+++ examples/ddragon.py
@@ -0,0 +1,10 @@
+from pyke import DataDragon
+
+# Let's create a DataDragon instance, we can pass a version if we want
+# But by default the latest version will be used
+# If you wish to pass a version use the following format:
+# "15.21.1" (or "lolpatch_7.20" for older versions)
+ddragon = DataDragon()
+
+print(ddragon.versions.get_all_versions())
+print(ddragon.champions.get_all_champions("en_US"))
