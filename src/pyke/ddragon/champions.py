ac2ca28d02e267d82e825f88741a923979af64b5
diodemusic <diodemusicuk@gmail.com>
2025-11-01 19:33:37 +0000

    Fix return type and use enum value

--- src/pyke/ddragon/champions.py
+++ src/pyke/ddragon/champions.py
@@ -1,3 +1,7 @@
+from typing import Any
+
+from pyke import Locale
+
 from .._base_data_dragon_client import _BaseDataDragonClient
 
 
@@ -5,8 +9,8 @@ class ChampionsEndpoint:
     def __init__(self, client: _BaseDataDragonClient):
         self._client = client
 
-    def get_all_champions(self, locale: str) -> list[str]:
-        path = f"/cdn/{self._client.version}/data/{locale}/champion.json"
+    def get_all_champions(self, locale: Locale) -> dict[str, Any]:
+        path = f"/cdn/{self._client.version}/data/{locale.value}/champion.json"
         data = self._client._data_dragon_request(path)
 
         return data
