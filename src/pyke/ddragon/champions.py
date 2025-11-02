6a094e357dcba1de3dcf963ec84b060fea63e8c0
diodemusic <diodemusicuk@gmail.com>
2025-11-02 17:00:39 +0000

    Add get_all_champions_full endpoint in ddragon.py

--- src/pyke/ddragon/champions.py
+++ src/pyke/ddragon/champions.py
@@ -1,7 +1,5 @@
 from typing import Any
 
-from pyke import Locale
-
 from .._base_data_dragon_client import _BaseDataDragonClient
 
 
@@ -9,20 +7,38 @@ class ChampionsEndpoint:
     def __init__(self, client: _BaseDataDragonClient):
         self._client = client
 
-    def get_all_champions(self, locale: Locale) -> dict[str, Any]:
+    def get_all_champions(self, locale: str) -> dict[str, Any]:
         """# Get all champions by locale
 
         **Example:**  
             `champions = ddragon.champions.get_all_champions(Locale.united_kingdom)`
 
         **Args:**  
-            `locale (Locale)` [Locale](/pyke/pyke.html#Locale) to use.  
+            `locale (str)` Locale to use.  
 
         **Returns:**  
             `dict[str, any]`
         """  # fmt: skip
 
-        path = f"/cdn/{self._client.version}/data/{locale.value}/champion.json"
+        path = f"/cdn/{self._client.version}/data/{locale}/champion.json"
+        data = self._client._data_dragon_request(path)
+
+        return data
+
+    def get_all_champions_full(self, locale: str) -> dict[str, Any]:
+        """# Get all champions by locale
+
+        **Example:**  
+            `champions = ddragon.champions.get_all_champions_full(Locale.united_kingdom)`
+
+        **Args:**  
+            `locale (str)` Locale to use.  
+
+        **Returns:**  
+            `dict[str, any]`
+        """  # fmt: skip
+
+        path = f"/cdn/{self._client.version}/data/{locale}/championFull.json"
         data = self._client._data_dragon_request(path)
 
         return data
