b5fb3bfb17252c14d2fabf082d937cd008d3d420
diodemusic <diodemusicuk@gmail.com>
2025-11-02 01:19:56 +0000

    Add docstrings

--- src/pyke/ddragon/champions.py
+++ src/pyke/ddragon/champions.py
@@ -10,6 +10,18 @@ class ChampionsEndpoint:
         self._client = client
 
     def get_all_champions(self, locale: Locale) -> dict[str, Any]:
+        """# Get all champions by locale
+
+        **Example:**  
+            `champions = ddragon.champions.get_all_champions(Locale.united_kingdom)`
+
+        **Args:**  
+            `locale (Locale)` [Locale](/pyke/pyke.html#Locale) to use.  
+
+        **Returns:**  
+            `dict[str, any]`
+        """  # fmt: skip
+
         path = f"/cdn/{self._client.version}/data/{locale.value}/champion.json"
         data = self._client._data_dragon_request(path)
 
--- src/pyke/ddragon/versions.py
+++ src/pyke/ddragon/versions.py
@@ -6,6 +6,15 @@ class VersionsEndpoint:
         self._client = client
 
     def get_all_versions(self) -> list[str]:
+        """# Get all valid Data Dragon versions
+
+        **Example:**  
+            `versions = ddragon.versions.get_all_versions()`
+
+        **Returns:**  
+            `list[str]` List of versions.
+        """  # fmt: skip
+
         path = "/api/versions.json"
         data = self._client._data_dragon_request(path)
 
