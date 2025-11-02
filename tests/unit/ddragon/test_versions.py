867c93b65c86dd416e340b20963b64f6a133c47a
diodemusic <diodemusicuk@gmail.com>
2025-11-02 17:44:23 +0000

    Add get_all_versions test

---
+++ tests/unit/ddragon/test_versions.py
@@ -0,0 +1,15 @@
+from pyke import DataDragon
+
+from .base import TEST_REGION, ddragon
+
+
+def test_get_all_versions(ddragon: DataDragon):
+    get_all_versions = ddragon.versions.get_all_versions()
+
+    assert isinstance(get_all_versions, list)
+    assert len(get_all_versions) > 0
+
+    for version in get_all_versions:
+        assert isinstance(version, str)
+
+    assert get_all_versions[0] == ddragon._client.version
