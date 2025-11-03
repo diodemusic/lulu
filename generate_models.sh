657c071f702c6d4727cb68403bb4f8cd0b6a69f1
diodemusic <diodemusicuk@gmail.com>
2025-11-03 04:11:00 +0000

    Remove generate_models.sh

--- generate_models.sh
+++
@@ -1,9 +0,0 @@
-python3 src/pyke/_schema/get_schema_json.py
-
-datamodel-codegen \
-  --input src/pyke/_schema/schema.json \
-  --input-file-type openapi \
-  --output src/pyke/models/ \
-  --snake-case-field
-
-black .
