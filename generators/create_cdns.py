5e36692904566029c929a2f3588b8f0aa5c7651a
diodemusic <diodemusicuk@gmail.com>
2025-11-03 04:11:29 +0000

    Initial commit

---
+++ generators/create_cdns.py
@@ -0,0 +1,57 @@
+import logging
+import os
+
+from caseconverter import snakecase, titlecase  # type: ignore
+
+from pyke import DataDragon
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+    datefmt="%H:%M:%S",
+)
+
+ddragon = DataDragon()
+version = ddragon._client.version
+dirname = os.path.dirname(__file__)
+path = os.path.join(dirname, f"./dragontail-{version}/{version}/data/en_GB/")
+
+files = os.listdir(path)
+
+for file in files:
+    if (
+        not os.path.isdir(os.path.join(path, file))
+        and not file.startswith("tft")
+        and file != ".DS_Store"
+    ):
+        file = snakecase(file.replace(".json", "").replace("-", ""))  # type: ignore
+
+        my_path = os.path.abspath(os.path.dirname(__file__))
+        path = os.path.join(my_path, f"../src/pyke/ddragon/{file}.py")
+
+        with open(path, "w") as f:
+            content = f'''from typing import Any
+
+from .._base_data_dragon_client import _BaseDataDragonClient
+
+
+class {titlecase(file).replace(" ", "")}Data:
+    def __init__(self, client: _BaseDataDragonClient):
+        self._client = client
+
+    def get_all(self, locale: str) -> dict[str, Any]:
+        """# Get all {file} by locale
+
+        **Example:**  
+            `{file} = ddragon.{file}.get_all(Locale.united_kingdom)`
+
+        **Args:**  
+            `locale (str)` Locale to use.  
+
+        **Returns:**  
+            `dict[str, any]`
+        """  # fmt: skip
+
+        return self._client._data_dragon_cdn_request(locale, "{file}")
+'''
+            f.write(content)
