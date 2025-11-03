e05d3a5f4d50296715218d2537c7b89e8812252e
diodemusic <diodemusicuk@gmail.com>
2025-11-03 04:12:00 +0000

    Add get_schema_json.py

---
+++ generators/get_schema_json.py
@@ -0,0 +1,16 @@
+import json
+
+import requests
+
+url = "http://www.mingweisamuel.com/riotapi-schema/openapi-3.0.0.json"
+
+response = requests.get(url, timeout=10)
+
+if response.status_code == 200:
+    json_content = response.json()
+
+    with open("./pyke/generators/schema.json", "w") as f:
+        json.dump(json_content, f, indent=4)
+else:
+    print(f"Error: {response.status_code}")
+    quit()
